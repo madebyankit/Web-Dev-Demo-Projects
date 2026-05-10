4dee0c437ea808a50650ffb2d2c9af95ce42c4de
yelloworld-design <ankitdabas1999@gmail.com>
2026-05-10 14:39:08 +0530

    feat: add endpoints for retrieving books by title and category

--- backend/fastapi/books-api/main.py
+++ backend/fastapi/books-api/main.py
@@ -18,3 +18,21 @@ async def root():
 @app.get('/books')
 async def read_all_books():
     return BOOKS
+
+@app.get('/books/{book_title}')
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():  
+            #casefold() is used to make the search case-insensitive
+            #.get() is used to avoid KeyError if 'title' key is missing in any book dictionary
+            return book
+    return {"message": "Book not found."}
+
+@app.get('/books/')
+async def read_category_by_query(category: str):
+    #This endpoint allows us to filter books by category using a query parameter.
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
